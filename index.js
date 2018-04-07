a77a13d8573e7f39be7c54b8b716d73ca975df22
Mitra Ardron <mitra@mitra.biz>
2018-04-07 20:19:26 +1000

    Reorganize files around simpler pattern

---
+++ index.js
@@ -0,0 +1,9 @@
+// Order is significant as should search earlier ones first
+// put IPFS before Webtorrent for showcasing, as Webtorrent works in some cases IPFS doesnt so that way we exercise both
+const DwebTransports = require("./Transports.js");
+require("./TransportHTTP.js");   // Can access via window.DwebTransports._transportclasses["HTTP"]
+require("./TransportIPFS.js");
+require("./TransportYJS.js");
+require("./TransportWEBTORRENT.js");
+window.DwebTransports = DwebTransports;
+exports = module.exports = DwebTransports;
