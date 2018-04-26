37e9d9507fdcb141845eb1704385b5a334e5c6d7
Mitra Ardron <mitra@mitra.biz>
2018-04-26 12:53:42 +1000

    Move build to webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  entry: {
+    'dweb-transports': './index.js',
+  },
+  output: {
+    filename: '[name]-bundle.js',
+    path: __dirname + '/dist'
+  },
+    node: {
+        fs: 'empty',
+        net: 'empty',
+        tls: 'empty',
+        crypto: 'empty',
+        process: true,
+        module: false,
+        clearImmediate: false,
+        Buffer: true,
+        setImmediate: false,
+        console: false
+    },
+
+    resolve: {
+        alias: {
+            zlib: 'browserify-zlib-next'
+        }
+    }
+}
