8a18c03888d71db31517f0a07f60da3505b42f05
Mitra Ardron <mitra@mitra.biz>
2019-11-22 16:36:47 +1100

    Refactor Naming.js to handle querys

--- Naming.js
+++ Naming.js
@@ -15,69 +15,67 @@ const archiveOrg = { // Mapping from archive.org URLs to dweb
     "search": ["https://dweb.archive.org/archive/archive.html?query="]
 }
 const domains = {
-  'dweb:': {
-    arc: { "archive.org": archiveOrg },
+  'dweb:/': {
+    arc: {"archive.org": archiveOrg},
     ipfs: ["http://ipfs.io/ipfs/", "https://dweb.me/ipfs/"], //TODO maybe need way to say check ipfs: as well ?
   },
+  "https://archive.org/": archiveOrg,
+  "http://archive.org/": archiveOrg,
+  "https://dweb.archive.org/": archiveOrg
+};
+
+function _toUrl(partialUrl, remainder, query) {
+  const p = partialUrl + remainder.join('/')
+  return query
+    ? [p,query].join(p.includes('?') ? '&' : '?')
+    : p;
 }
 
 /**
- * Interpret the string found in domain table
- * @param remainder ARRAY   path split on '/'
- * @returns depending on partialUrl
- *  ANY.html => [ ANYhtml, REMAINDER
- *  ANY?x=   => ANY?x=REMAINDER
- *  ANY/     => ANY/REMAINDER
-  */
-function expand(partialUrl, remainder) {
-  return partialUrl.endsWith("html")
-  ? [partialUrl, remainder.join('/')] // THis might always be an error.
-  : partialUrl.endsWith("=")
-  ? partialUrl + remainder.join('/')
-  : (partialUrl.endsWith("/"))
-  ? partialUrl+remainder.join('/')
-  : undefined;
-}
-function resolve(parent, table, path) {
-  /**
-   * parent = [ ]   path components matched so far e.g. [ "dweb:" ] or ["https://archive.org", "metadata" ]
-   * table = { key: url || [url]}
-   * path = "d/e/f"
-   * returns [ url || undefined ]
-   */
-  //debug("Resolving %o in %s", path, parent);
-  const remainder = Array.isArray(path) ? path : path.split('/');
-  const name = remainder.shift();
+ * Recursive worker
+ * @param parent string
+ * @param domain object { key: [urlFrag] or key: urlFrag}
+ * @param pathArr array
+ * @returns [URL]
+ * @private
+ */
+function _recursive(parent, table, pathArr) {
+  const name = pathArr.shift();
   let found = table[name];
+  if (!found) {
+    pathArr.unshift(name); // Didn't use it - pass to return or "."
+  }
   if (!found && table["."]) {
-    remainder.unshift(name); // Didn't use it - pass to "."
-    found = table["."];
+    found = table["."]; // Maybe undefined
   }
-  if (found) {
-    if (Array.isArray(found)) {
-      return (found.map(partialUrl => expand(partialUrl, remainder)).filter(url => !!url)); // [url || [url, remainder]]
-    } else if (typeof found === "object") {
-      return resolve([parent, name].join('/'), found, remainder);
-    } else if (typeof found === "string") {
-      return [ expand(found, remainder) ]
-    }
-  } else {
-    debug("WARNING unable to resolve %s in %s", name, parent.join('/') || '/' )
-    return undefined; // Remainder not found
+  if (!found) {
+    debug("WARNING unable to resolve %s in %s", name, parent )
   }
+  return ((typeof found === "object") && !Array.isArray(found))
+    ? _recursive(parent + name + "/", found, pathArr)
+    : [ found, pathArr ];
 }
 
+/**
+ * Resolve path in a domain object
+ * @param parent
+ * @param domain
+ * @param pathAndQuery
+ * @returns {any}
+ */
+function resolveNameInDomain(parent, domain, pathAndQuery) {
+  const [path, query] = pathAndQuery.split('?');
+  const [foundStrOrArr, remainder] = _recursive(parent, domain, path.split('/')); // recursive
+  return Array.isArray(foundStrOrArr)
+    ? foundStrOrArr.map(partialUrl => _toUrl(partialUrl, remainder, query))
+      .filter(url =>  !!url)
+    : [ _toUrl(foundStrOrArr, remainder, query) ];
+}
 function resolveName(url) {
-  return url.startsWith("dweb:/")
-    ? resolve(["dweb:"], domains["dweb:"], url.slice(6))
-    : url.startsWith("https://archive.org/")
-    ? resolve(["https://archive.org"], archiveOrg, url.slice(20) )
-    : url.startsWith("http://archive.org/")
-    ? resolve(["https://archive.org"], archiveOrg, url.slice(19) )
-    : url.startsWith("https://dweb.archive.org/")
-    ? resolve(["https://dweb.archive.org"], archiveOrg, url.slice(25) )
-    : url; //
-
+  const dom = Object.keys(domains).find( d => url.startsWith(d));
+  return dom
+    ? resolveNameInDomain(dom, domains[dom], url.slice(dom.length))
+    : url;
 }
 function naming(names) {
     return [].concat(...names.map(n => resolveName(n)))
@@ -87,14 +85,13 @@ async function p_namingcb(names) {
 }
 
 /*
-//TODO find in DM where its catching http://dweb.me and heading back to http://localhost:4244
 const testdata = {
   "dweb:/arc/archive.org/metadata/foo": [
     "https://www-dweb-metadata.dev.archive.org/metadata/foo",
     "gun:/gun/arc/archive.org/metadata/foo",
     "wolk://dweb.archive.org/metadata/foo" ],
   "dweb:/arc/archive.org/details/foo": [
-    "https://dweb.me/archive/archive.html?item=foo"],
+    "https://dweb.archive.org/archive/archive.html?item=foo"],
   "https://archive.org/metadata/bar": [
     'wolk://dweb.archive.org/metadata/bar',
     'gun:/gun/arc/archive.org/metadata/bar',
@@ -102,6 +99,9 @@ const testdata = {
   ],
   "https://archive.org/something/else": [
     'https://archive.org/something/else'
+  ],
+  "https://archive.org/advancedsearch?query=splat": [
+    'https://dweb.archive.org/advancedsearch?query=splat'
   ]
 }
 
